97ece1deb88a2903945c105ce052230b5b1f4351
tayljs2 <tayljs2@student.op.ac.nz>
2026-05-14 15:36:04 +1200

    a

---
+++ gameproject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace gameproject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

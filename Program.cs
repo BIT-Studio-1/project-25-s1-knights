221f8dd797fbca2dff5ea3f6635db921d9406426
tayljs2 <tayljs2@student.op.ac.nz>
2026-05-14 16:05:27 +1200

    c

--- Program.cs
+++ Program.cs
@@ -5,6 +5,32 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            
+        }
+
+        public static void Luke()
+        {
+
+        }
+
+        public static void Tim()
+        {
+
+        }
+
+        public static void Arjun()
+        {
+
+        }
+
+        public static void Stephanie()
+        {
+
+        }
+
+        public static void James()
+        {
+
         }
     }
 }

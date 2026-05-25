f50ac3338351760de83d767cc10be7bab8572e56
tayljs2 <tayljs2@student.op.ac.nz>
2026-05-26 11:14:13 +1200

    start of intro screen

--- Intro.cs
+++ Intro.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.Runtime.InteropServices;
+using static gameproject.Globals;
+using static System.Console;
+using static System.ConsoleKey;
+using static System.Math;
+using System.Diagnostics;
+using static gameproject.Character;
+using static gameproject.Lives;
+using static gameproject.invaders;
+using static gameproject.Levels;
+using static gameproject.Menu;
 namespace gameproject
 {
     internal class Intro
     {
+       public static void initialScreen()
+        {
+            string text = "PRESS ANY KEY TO BEGIN";
+            SetCursorPosition((consoleWidth/2 - (text.Length/2)), consoleHeight/2);
+            Write(text);
+            ReadKey(true);
+            introScreen();
+        }
+
+        static void introScreen()
+        {
+            Thread.Sleep(100);
+            menuStart = true;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ using static gameproject.Lives;
 using static gameproject.invaders;
 using static gameproject.Levels;
 using static gameproject.Menu;
-
+using static gameproject.Intro;
 
 
 
@@ -36,7 +36,7 @@ namespace gameproject
 
         public static int bottomRow = WindowHeight - 1, farRow = WindowWidth - 1, playerX = WindowWidth / 2, playerY = WindowHeight - 5;
         public static HashSet<ConsoleKey> PressedKeys = new HashSet<ConsoleKey>();
-        public static bool start = false, moved = false, menuStart = true;
+        public static bool start = false, moved = false, menuStart = false;
 
         public static List<Bullet> PlayerBullets = new List<Bullet>(); //creates the list to hold the bullets - saw this on reddit
 
@@ -73,8 +73,8 @@ namespace gameproject
         static async Task Main()
         {
             CursorVisible = false;
-            
 
+            initialScreen();
             startmenu();
             
             

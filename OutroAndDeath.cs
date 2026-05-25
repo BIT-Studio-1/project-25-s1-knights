6392fcb33fbe8aeaa14b1ecc6a7dea0236c9526d
SVILLA-622 <soldso1@student.op.ac.nz>
2026-05-26 11:42:15 +1200

    Added ASCII win screen to OutroAndDeath

--- OutroAndDeath.cs
+++ OutroAndDeath.cs
@@ -10,12 +10,30 @@ namespace gameproject
     {
         public static void ShowWin()
         {
-            
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine(@"____    ____  ______    __    __     ____    __    ____  __  .__   __. 
+\   \  /   / /  __  \  |  |  |  |    \   \  /  \  /   / |  | |  \ |  | 
+ \   \/   / |  |  |  | |  |  |  |     \   \/    \/   /  |  | |   \|  | 
+  \_    _/  |  |  |  | |  |  |  |      \            /   |  | |  . `  | 
+    |  |    |  `--'  | |  `--'  |       \    /\    /    |  | |  |\   | 
+    |__|     \______/   \______/         \__/  \__/     |__| |__| \__| 
+                                                                       ");
+
+            Console.ResetColor();
+
+            Console.WriteLine();
+            Console.WriteLine("YOU WIN!");
+            Console.WriteLine("Press any key to continue....");
+
+            Console.ReadKey(true);
         }
 
         public static void ShowLose()
         {
-
+            Console.Clear();
+            Console.WriteLine("YOU LOSE!");
         }
 
     }

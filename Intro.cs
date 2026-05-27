a5b7cf6e84046f47a5200b8584d6a9f475757682
Luke Bond (1000139488) <BONDLI1@student.op.ac.nz>
2026-05-27 17:16:31 +1200

    Start wars theme

--- Intro.cs
+++ Intro.cs
@@ -25,6 +25,8 @@ namespace gameproject
             ReadKey(true);
             Clear();
             introScreen();
+            StarWarsTheme();
+            new Thread(StarWarsTheme) { IsBackground = true }.Start();
         }
 
         static void introScreen()
@@ -33,6 +35,29 @@ namespace gameproject
             Write("\r\n\r\n ____  _                _   _       _     _           \r\n|  _ \\| | __ _  ___ ___| | | | ___ | | __| | ___ _ __ \r\n| |_) | |/ _` |/ __/ _ \\ |_| |/ _ \\| |/ _` |/ _ \\ '__|\r\n|  __/| | (_| | (_|  __/  _  | (_) | | (_| |  __/ |   \r\n|_|   |_|\\__,_|\\___\\___|_| |_|\\___/|_|\\__,_|\\___|_|   \r\n\r\n");
             Thread.Sleep(1200);
             menuStart = true;
+        }
+        public static void StarWarsTheme()
+        {
+            // Star Wars Main Theme
+            Beep(440, 500);   // A
+            Beep(440, 500);   // A
+            Beep(440, 500);   // A
+            Beep(349, 350);   // F
+            Beep(523, 150);   // C
+            Beep(440, 500);   // A
+            Beep(349, 350);   // F
+            Beep(523, 150);   // C
+            Beep(440, 1000);  // A
+
+            Beep(659, 500);   // E
+            Beep(659, 500);   // E
+            Beep(659, 500);   // E
+            Beep(698, 350);   // F
+            Beep(523, 150);   // C
+            Beep(415, 500);   // G#
+            Beep(349, 350);   // F
+            Beep(523, 150);   // C
+            Beep(440, 1000);  // A
         }
     }
 }

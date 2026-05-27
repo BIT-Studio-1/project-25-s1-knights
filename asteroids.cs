c1daab1dcaa9984ac6f9612a800c390c2f6b932d
Timokevo1 <Timokevo@gmail.com>
2026-05-28 11:32:58 +1200

    Adding global/other classes to asteroids

--- asteroids.cs
+++ asteroids.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using static gameproject.Globals;
+using static System.Console;
+using static System.ConsoleKey;
+using static System.Math;
+using System.Diagnostics;
+using static gameproject.Bullet;
+using static gameproject.Program;
+using static gameproject.Character;
+using static gameproject.Levels;
+using static gameproject.Menu;
 
 namespace gameproject
 {
     internal class asteroids
     {
+
+        public static void newAsteroids()
+        {
+
+        }
+
     }
 }
+
+
